712d47331482ea78324e244435478a80983523a9
Bogar Alejandro Vazquez Mata <31672532+Bogaralex135@users.noreply.github.com>
2023-11-29 21:15:16 -0600

    Add files via upload

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -3,6 +3,7 @@ import { useGetData } from '../hooks/useGetData'
 import { ProductCard } from '../components/ProductCard'
 import { useParams } from 'react-router-dom'
 import { CategoryHeader } from '../components/CategoryHeader'
+import { Loading } from '../layouts/Loading'
 
 export function CategoryPage() {
   const { category } = useParams()
@@ -13,7 +14,7 @@ export function CategoryPage() {
   return (
     <>
       <CategoryHeader category={category} />
-      {loading && <p>Loading...</p>}
+      {loading && <Loading />}
       {error && <p>{error}</p>}
       <ul className='grid grid-cols-autoColums justify-center items-center px-10 gap-5 md:gap-20 md:px-24 md:py-10'>
         {data?.map(product => (
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { useGetData } from '../hooks/useGetData'
 import { ProductCard } from '../components/ProductCard'
 import { CategoryHeader } from '../components/CategoryHeader'
+import { Loading } from '../layouts/Loading'
 
 export function Products() {
   const urlTest = 'https://fakestoreapi.com/products'
@@ -10,7 +11,7 @@ export function Products() {
 
   return (
     <>
-      {loading && <p>Loading...</p>}
+      {loading && <Loading />}
       {error && <p>{error}</p>}
       <CategoryHeader category='Todos los Productos' />
       <ul className='grid grid-cols-autoColums justify-center items-center px-10 gap-20 md:px-24 md:py-10'>
