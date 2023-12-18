f6a827b29e4e3b30dcb404a94236c8fa1f5b3f63
Bogar Alejandro Vazquez Mata <31672532+Bogaralex135@users.noreply.github.com>
2023-12-18 12:49:04 -0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,22 @@ export default {
       boxShadow: {
         default: '0 3px 10px 5px  rgba(0, 0, 0, 0.12)',
       },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        rajdhani: ['Rajdhani', 'sans-serif'],
+        raleway: ['Raleway', 'sans-serif'],
+      },
+      colors: {
+        palette: {
+          primary: '#152D2F',
+          secondary: '#529296',
+          tertiary: '#599A6A',
+          quaternary: '#8F5D3D',
+          quinary: '#FFC692',
+          senary: '#F6DCB1',
+          septenary: '#FFFAC2',
+        },
+      },
     },
   },
   plugins: [],
